38ecda1e3db5d7d783cd493f367f894727a4bb8e
hayatochigi <hayatochigi.shimane.pre117@gmail.com>
2020-12-02 13:25:31 +0900

    small partial change

--- Producer_Consumer_dotnet5/Producer_Consumer_dotnet5/Typedef.cs
+++ Producer_Consumer_dotnet5/Producer_Consumer_dotnet5/Typedef.cs
@@ -9,6 +9,8 @@ namespace Producer_Consumer_dotnet5
     internal class MssgData
     {
         public string Mssg { get; set; }
+     
+        // For extensible application, use "object" type.
         public object Data { get; set; }
     }
 }
