32a519469d7009ed15e785b0ea2b637855835a80
hayatochigi <hayatochigi.shimane.pre117@gmail.com>
2020-12-02 13:24:02 +0900

    Add Pro&Cons application

---
+++ Producer_Consumer_dotnet5/Producer_Consumer_dotnet5/ProsConsMain.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace Producer_Consumer_dotnet5
+{
+    class ProsConsMain
+    {
+        static void Main()
+        {
+            // In this application, single queue ref will be shared by multiple Processing Loop.
+            var mssgQueue = new BlockingCollection<MssgData>();
+
+            // Run each Processing Loop in other thraed.
+            Task<string> task_producer = Task.Run(() => { return ProducerLoop(mssgQueue); });
+            Task<string> task_consumer = Task.Run(() => { return ConsumerLoop(mssgQueue); });
+
+            Console.ReadKey();
+            mssgQueue.Dispose();
+
+            // Wait all loop completed.
+            Task.WhenAll(task_producer, task_consumer);
+
+            Console.WriteLine("Enter a Key to exit...");
+            Console.ReadKey();
+            
+        }
+
+        /// <summary>
+        /// Generate "Message & Data" and add them into Blocking Queue.
+        /// </summary>
+        /// <param name="queue"></param>
+        /// <returns></returns>
+        static async Task<string> ProducerLoop(BlockingCollection<MssgData> queue)
+        {
+            ShowThreadingId();
+            var _mssg = new MssgData();
+            var rand = new Random();
+            while(queue != null)
+            {
+                _mssg.Mssg = "From Producr";
+                _mssg.Data = rand.NextDouble();
+                queue.TryAdd(_mssg);
+                await Task.Delay(2000);
+               
+            }
+            return "";
+        }
+
+        /// <summary>
+        /// Consume enqueued data.
+        /// </summary>
+        /// <param name="queue"></param>
+        /// <returns></returns>
+        static string ConsumerLoop(BlockingCollection<MssgData> queue)
+        {
+            ShowThreadingId();
+            while (queue != null)
+            {
+                foreach(var _mssg in queue.GetConsumingEnumerable()) 
+                {
+                    Console.WriteLine("Message: {0} \t Data: {1:f2}", _mssg.Mssg, Convert.ToDouble(_mssg.Data));
+                }
+            }
+            return "";
+        }
+
+        /// <summary>
+        /// Display current thrad id.
+        /// </summary>
+        static void ShowThreadingId()
+        {
+            Console.WriteLine("Thread : {0}", Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
---
+++ Producer_Consumer_dotnet5/Producer_Consumer_dotnet5/Typedef.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Producer_Consumer_dotnet5
+{
+    internal class MssgData
+    {
+        public string Mssg { get; set; }
+        public object Data { get; set; }
+    }
+}
