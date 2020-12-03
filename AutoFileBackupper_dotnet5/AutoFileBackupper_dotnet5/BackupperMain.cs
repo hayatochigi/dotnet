3a52559188a6aaf6785915997bfa8b5474a7d1bd
hayatochigi <hayatochigi.shimane.pre117@gmail.com>
2020-12-03 18:15:36 +0900

    New application but under development

---
+++ AutoFileBackupper_dotnet5/AutoFileBackupper_dotnet5/BackupperMain.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Text.RegularExpressions;
+using AutoFileBackupper_dotnet5.Models;
+
+namespace AutoFileBackupper_dotnet5
+{
+    class Program
+    {
+        const string BckpDirectoryName = "old";
+
+        static void Main(string[] args)
+        {
+            string file_path = "C:\\Users\\shimane\\Desktop\\my.txt";
+
+            // Set variable for "File Name", "File Extention", "Source Directory" 
+            string file_name = Path.GetFileNameWithoutExtension(file_path);
+            string file_ext = Path.GetExtension(file_path);
+            string source_dir = Path.GetDirectoryName(file_path);
+
+            CheckBckpFile(source_dir);
+
+            // Do file copy operation.
+            //File.Copy(file_path, "New.txt");
+
+            Console.WriteLine("Enter a key to exit...");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Check Backup directory and if not, create as new.
+        /// Then, create new backup file.
+        /// </summary>
+        /// <param name="source_dir"></param>
+        /// <returns></returns>
+        static string CheckBckpFile(string source_dir)
+        {
+            // Create Backup path
+            string bckp_dir = source_dir + "\\" + BckpDirectoryName;
+
+            if (Directory.Exists(bckp_dir)){
+                foreach (var file in Directory.GetFiles(bckp_dir))
+                {
+                    string file_name = Path.GetFileNameWithoutExtension(file);
+                    var version = VersionManager.ToVerionInfoArray(Regex.Match(file_name, VersionManager.GetVersionFormat()).Value);
+                    foreach(int x in version)
+                    {
+                        Console.WriteLine("value = {0}", x);
+                    }
+                    
+                }
+            }
+            else
+            {
+                // Couldn't find a backup folder, create a new one.
+                Directory.CreateDirectory(bckp_dir);
+            }
+            return "";
+        }
+        
+
+    }
+}
---
+++ AutoFileBackupper_dotnet5/AutoFileBackupper_dotnet5/Models/VersionManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace AutoFileBackupper_dotnet5.Models
+{
+    internal class VersionManager
+    {
+        internal static string GetVersionFormat()
+        {
+            return "\\d{1,}_\\d{1,}_\\d{1,}"; 
+        }
+
+        /// <summary>
+        /// Convert Version string (ex, "1_2_3") to int array (ex, {1, 2, 3})
+        /// </summary>
+        /// <param name="ver">Input version string "%d_%d_%d"</param>
+        /// <returns>Converted version string to int[]</returns>
+        internal static IEnumerable<int> ToVerionInfoArray(string ver)
+        {
+            if (ver == "")
+            {
+                // Return default version info.
+                return new[] { 1, 0, 0 };
+            }
+            else
+            {
+
+                //return Array.ConvertAll(Regex.Split(ver, "_"), s => int.Parse(s));
+                return null;
+            }
+        }
+    }
+}
